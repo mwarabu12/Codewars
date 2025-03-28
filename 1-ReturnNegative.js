08077c8244df36927fe83943168f141e929a318b
mwarabu12 <eric.mwarabu2@gmail.com>
2025-03-28 15:37:27 +0200

    Returning a negative value

---
+++ 1-ReturnNegative.js
@@ -0,0 +1,10 @@
+const makeNegative = (num) => {
+    if(num<0){
+      return(num)
+    }else if(num>0){
+      return(-num)
+    } else{
+      return(0)
+    }
+  }
+  console.log(makeNegative (-22));
