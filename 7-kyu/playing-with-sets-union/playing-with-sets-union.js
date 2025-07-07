014579d4534aa7e57ee952f556960aa5dca4523d
Eric Nshimiyimana Mwarabu <eric.mwarabu2@gmail.com>
2025-07-07 18:09:51 +0200

    Update playing-with-sets-union.js
    This kata takes in 2 Sets and return their union (combination)

--- 7-kyu/playing-with-sets-union/playing-with-sets-union.js
+++ 7-kyu/playing-with-sets-union/playing-with-sets-union.js
@@ -1,4 +1,4 @@
 function union(s1, s2) {
-  return new Set([...s1, ...s2]);
+  return new Set([...s1, ...s2]);//Here, I first combined the 2 sets into one by spread elements from both Sets using the spread operator (...) which automatically keeps only unique elements.
 }
 ​
