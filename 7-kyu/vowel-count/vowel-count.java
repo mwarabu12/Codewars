bd5b810229bc4a646c95d5280f8fbf8c4f631f8d
Eric Mwarabu Nshimiyimana <eric.mwarabu2@gmail.com>
2025-11-14 13:35:52 +0200

    Add solution - CodeHub

---
+++ 7-kyu/vowel-count/vowel-count.java
@@ -0,0 +1,14 @@
+public class Vowels {
+    public static int getCount(String str) {
+        int count = 0;
+​
+        for (char c : str.toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                count++;
+            }
+        }
+​
+        return count;
+    }
+}
+​
