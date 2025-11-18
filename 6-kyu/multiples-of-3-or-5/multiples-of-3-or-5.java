dcbbd3ba2c4bbec28f4ab622013b90b5cf6bb186
Eric Mwarabu Nshimiyimana <eric.mwarabu2@gmail.com>
2025-11-18 11:06:42 +0200

    Add solution - CodeHub

---
+++ 6-kyu/multiples-of-3-or-5/multiples-of-3-or-5.java
@@ -0,0 +1,12 @@
+public class Solution {
+​
+  public int solution(int number) {
+    int total = 0;
+    for (int i = 0; i < number; i++){
+     if (i % 3 == 0 || i % 5 == 0){
+       total += i;
+     }
+    }
+    return total;
+  }
+}
