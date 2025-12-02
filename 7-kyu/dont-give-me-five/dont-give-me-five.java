79a48fe5f34b6ddf90ac58baed1a75f2cc159177
Eric Mwarabu Nshimiyimana <eric.mwarabu2@gmail.com>
2025-12-02 18:33:44 +0200

    Add solution - CodeHub

---
+++ 7-kyu/dont-give-me-five/dont-give-me-five.java
@@ -0,0 +1,17 @@
+public class Kata {
+    public static int dontGiveMeFive(int start, int end) {
+        
+      int count = 0;
+​
+        for (int i = start; i <= end; i++){
+            if (!String.valueOf(i).contains("5")){
+                      
+                count++;
+            }
+        }
+     
+      
+        return count;
+    }
+}
+​
