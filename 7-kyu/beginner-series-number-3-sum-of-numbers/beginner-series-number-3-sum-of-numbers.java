9f659ab9091e32e823e6200551228bc2887587f3
Eric Mwarabu Nshimiyimana <eric.mwarabu2@gmail.com>
2025-12-09 11:11:47 +0200

    Add solution - CodeHub

---
+++ 7-kyu/beginner-series-number-3-sum-of-numbers/beginner-series-number-3-sum-of-numbers.java
@@ -0,0 +1,16 @@
+  public class Sum
+  {
+     public int GetSum(int a, int b)
+     {
+      //Good luck!
+       
+       int sum = 0;
+       
+       for (int i = Math.min(a, b); i <= Math.max(a, b); i++){
+          sum += i;
+     }
+               
+      return sum;
+     }
+  }
+​
