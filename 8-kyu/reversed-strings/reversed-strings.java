b5356be0328a122f2b94bdb881a0fd57d5b195fe
Eric Mwarabu Nshimiyimana <eric.mwarabu2@gmail.com>
2025-12-14 22:54:10 +0200

    Add solution - CodeHub

---
+++ 8-kyu/reversed-strings/reversed-strings.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Kata {
+​
+  public static String solution(String str) {
+    StringBuilder result = new StringBuilder();
+    return result.append(str).reverse().toString();
+  }
+​
+}
