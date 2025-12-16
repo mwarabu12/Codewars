928f88388b1e38ec50d4de50da78a0781da2da0e
Eric Mwarabu Nshimiyimana <eric.mwarabu2@gmail.com>
2025-12-16 11:48:50 +0200

    Add solution - CodeHub

---
+++ 6-kyu/stop-gninnips-my-sdrow/stop-gninnips-my-sdrow.java
@@ -0,0 +1,18 @@
+public class SpinWords {
+​
+  public String spinWords(String sentence) {
+    //TODO: Code stuff here
+    
+    String[] words = sentence.split(" ");
+    
+    for (int i = 0; i < words.length; i++) {
+      if (words[i].length() >= 5) {
+        
+        words[i] = new StringBuilder(words[i]).reverse().toString();
+      }
+    }
+    
+    return String.join(" ", words);
+  }
+}
+​
