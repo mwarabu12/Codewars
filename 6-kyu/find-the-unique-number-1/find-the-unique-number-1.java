82172e83c7d00398a59d83444f286da7c4e162c1
Eric Mwarabu Nshimiyimana <eric.mwarabu2@gmail.com>
2025-12-18 13:17:40 +0200

    Update solution - CodeHub

--- 6-kyu/find-the-unique-number-1/find-the-unique-number-1.java
+++ 6-kyu/find-the-unique-number-1/find-the-unique-number-1.java
@@ -1,21 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+​
 public class Kata {
-    public static double findUniq(double arr[]) {
+​
+    public static double findUniq(double[] arr) {
       // Do the magic
       
-      double common;
-      if (arr[0] == arr [1] || arr[0] == arr[2]){
-        common = arr [0];
-      } else {
-        common = arr [1];
-      }
-      
-      for (double num : arr){
-        if (num != common){
-          return num;
+        Map<Double, Integer> countMap = new HashMap<>();
+​
+        for (double num : arr) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
-      }
-      
-      return 0;
+​
+        for (double num : arr) {
+            if (countMap.get(num) == 1) {
+                return num;
+            }
+        }
+​
+        return 0;
     }
-   
+  
 }
+​
